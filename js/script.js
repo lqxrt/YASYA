93b7ab1dcd60d5d48a675cfcded5e9393c2caf23
SivaPrasad43 <39110552+SivaPrasad43@users.noreply.github.com>
2023-04-13 08:02:27 +0530

    nav-bar-icon-issue:solved and footer

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const navOptions = document.getElementById("nav-options")
 const navHambIcon = document.getElementById("nav-hamb-icon")
-var iconImg = document.getElementById("nav-hamb-img");
+var icon = document.getElementById("nav-hamb");
 function navBar(){
     console.log("helo")
     if (navOptions.className === "nav-options") {
         console.log("true")
         navOptions.className+= " visible"
-        iconImg.src = "./assets/x-solid.svg";
+        icon.className = "fa fa-times";
     } else {
         console.log("false")
         navOptions.className = "nav-options"
-        iconImg.src = "./assets/bars-solid.svg";
+        icon.className = "fa fa-bars";
       }
 }

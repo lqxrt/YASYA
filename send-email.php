997e99aef74215f9306da2f66339a9b6baf2d055
SivaPrasad43 <39110552+SivaPrasad43@users.noreply.github.com>
2023-04-19 02:22:51 +0530

    contact-formspree

---
+++ send-email.php
@@ -0,0 +1,20 @@
+<?php
+// Get the form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Create the email message
+$to = 'sivaprasadshines43@gmail.com'; // Replace with your own email address
+$subject = 'New Contact Form Submission';
+$body = "Name: $name\nEmail: $email\nMessage:\n$message";
+
+// Send the email
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+if (mail($to, $subject, $body, $headers)) {
+    echo 'success';
+} else {
+    echo 'error';
+}
+?>
